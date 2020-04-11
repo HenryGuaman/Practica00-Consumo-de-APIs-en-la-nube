b32772a1f651f6572a5b1cf629b08d43b6e5bd89
Henry <henry96guaman@gmail.com>
2020-04-10 21:21:09 -0500

    Metodos de buscar por id

--- funciones.js
+++ funciones.js
@@ -22,7 +22,7 @@ function buscarPeliculaPorTitulo(){
                     // Log each movie's info
                     console.log(movie.Title)
                     detalles += "<tr>"+
-                        //"<td><a href='#' onclick=\"buscarPeliculaPorId('" + movie.imdbID + "')\"><td>"+
+                        "<td><a href='#' onclick=\"buscarPeliculaPorId('" + movie.imdbID + "')\">Mas Detalles"+
                         "<td>"+movie.imdbID+
                         "<td>"+movie.Title+
                         "<td>"+movie.Year+
@@ -39,3 +39,41 @@ function buscarPeliculaPorTitulo(){
     }
     return false;  
 }
+
+function buscarPeliculaPorId(iden){
+   
+    var detalles="";
+    if(iden == ""){
+        detalles = "Sin informacion disponible...."
+        document.getElementById("informacion").innerHTML = detalles;
+
+    }else{
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest();
+        }else{
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                var data = JSON.parse(this.responseText)
+
+                var x;  
+          
+                  for(x in data){
+                      detalles+=                       
+                       "<tr>"+data[x] +"</tr>" +"<br>" 
+                       "</tr>"                     ;       
+                       }   
+              }
+              var imagen= "<img src=\'"+data.Poster+"'/ ></img> "     
+              
+              document.getElementById("detalles").innerHTML=detalles;
+              document.getElementById("imagen").innerHTML=imagen;
+  
+          }; 
+          xmlhttp.open("GET","http://www.omdbapi.com/?&apikey=4e291117&s="+ id +"&plot=full",true);
+          
+          xmlhttp.send();
+      }
+}  
+var pagina=2;
